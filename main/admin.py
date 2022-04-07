457eab49135849f469d3ac1e580566956004ccc6
bentnn <kirill.ignatyev2013@gmail.com>
2022-04-07 22:01:40 +0300

    added achievements to db

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Achievement, AboutUser
 
-# Register your models here.
+admin.site.register(Achievement)
+admin.site.register(AboutUser)
--- main/models.py
+++ main/models.py
@@ -2,6 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Achievement(models.Model):
+	name = models.CharField(max_length=70)
+	icon = models.ImageField(upload_to="Achievements")
+
+	class Meta:
+		verbose_name = 'достижение'
+		verbose_name_plural = 'достижения'
+
+	def __str__(self):
+		return self.name
+
+
 class AboutUser(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	level = models.SmallIntegerField(default=0)
@@ -9,3 +21,11 @@ class AboutUser(models.Model):
 	money = models.IntegerField(default=0)
 	avatar = models.ImageField(upload_to="Avatars", blank=True)
 	subs = models.ManyToManyField(User, default=[], related_name="subs_to")
+	achievements = models.ManyToManyField(Achievement, default=[], related_name="achieve_owner")
+
+	class Meta:
+		verbose_name = 'данные пользователя'
+		verbose_name_plural = 'данные пользователей'
+
+	def __str__(self):
+		return self.user.username
