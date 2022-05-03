2e05c2fe44d5244d8dc1ee30c25d649df779e99d
Wisdou <51473681+Wisdou@users.noreply.github.com>
2022-05-03 17:04:13 +0300

    Add pages for course

--- course/views.py
+++ course/views.py
@@ -4,13 +4,12 @@ from django.contrib.auth.decorators import login_required
 
 
 @login_required(login_url='login')
-<<<<<<< HEAD
 def index(request):
     return render(request, 'course/content/index.html')
 
 @login_required(login_url='login')
-def coursepage(request, str):
-    return render(request, 'course/content/pages/' + str + '.html')
+def coursepage(request, name):
+    return render(request, 'course/content/pages/' + name + '.html')
 
 @login_required(login_url='login')
 def tasks(request):
@@ -19,11 +18,3 @@ def tasks(request):
 @login_required(login_url='login')
 def task(request):
     return render(request, 'course/tasks/taskpage.html')
-=======
-def test(request):
-    return HttpResponse("<h1>Test course</h1>")
-
-
-def home(request):
-    return HttpResponse("<h1>Root of course app</h1>")
->>>>>>> master
