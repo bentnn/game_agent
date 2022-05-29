268742bd35be9482cf6f6a8ee39fbc3b277a0a21
bentnn <kirill.ignatevv@gmail.com>
2022-05-29 19:26:24 +0300

    change_front

--- game_agent/settings.py
+++ game_agent/settings.py
@@ -15,13 +15,13 @@ from pathlib import Path
 from django.contrib.messages import constants as messages
 
 
-# MESSAGE_TAGS = {
-#     messages.DEBUG: 'alert-secondary',
-#     messages.INFO: 'alert-info',
-#     messages.SUCCESS: 'alert-success',
-#     messages.WARNING: 'alert-warning',
-#     messages.ERROR: 'alert-danger',
-# }
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-secondary',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
